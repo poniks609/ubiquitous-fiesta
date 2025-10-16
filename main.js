527331ba5be2915d93d02967536a5f9491ef8932
poniks609 <haruto2010haba@gmail.com>
2025-10-16 02:30:52 +0000

    commited

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-var foo = require('./foo.js');
-console.log(foo(5));
+var a = document.getElementById("a");
+const axios = require('axios');
+
+async function fetchData() {
+  try {
+    const response = await axios.get('https://example.com');
+    a.innerHTML = response.data;
+  } catch (error) {
+    a.innerHTML = 'Error fetching data:', error;
+  }
+}
+
+fetchData();
